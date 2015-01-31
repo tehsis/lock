b7eccb6b9797513669a32c917002635e4c57c788
Pablo Terradillos <tehsis@gmail.com>
2015-01-31 03:56:16 -0300

    Added recaptcha to login screen

--- test/recaptcha.tests.js
+++ test/recaptcha.tests.js
@@ -2,10 +2,8 @@ describe('Recaptcha', function() {
   it('should load the recaptcha script', function(done) {
     var $recaptcha_element = $('<div class="recaptcha-container">');
     var recaptcha = new Recaptcha($recaptcha_element[0], {
-      recaptcha: {
         key: '6LeCEAETAAAAACx-QSVnbOrTvF6S2NaK1bjkslhJ',
         theme: 'dark'
-      }
     }, function() {
         // Detect when when recaptcha was rendered
         expect($recaptcha_element.children('iframe')).to.not.be(0);
